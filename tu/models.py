792599a88691f070e3389cc08753ffef6f6cc0e2
lionliccy <swlymoure@outlook.com>
2019-06-19 15:46:54 +0800

    数据库访问

--- tu/models.py
+++ tu/models.py
@@ -1,3 +1,18 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class TODO(models.Model):
+    thing=models.CharField(max_length=20,verbose_name='待办事项')
+    done=models.BooleanField(default=False,verbose_name='已完成')
+    def __str__(self):
+        return self.thing
+
+    class Meta():
+        db_table='todolist'
+
+class Student(models.Model):
+    number=models.IntegerField()
+    name=models.CharField(max_length=10,null=False)
+    apartment=models.TextField()
+    birthday=models.DateField(default=date.today)
--- tu/views.py
+++ tu/views.py
@@ -1,51 +1,49 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+from tu.models import TODO
+
 lit=[
     {'待办事项':'吃饭','已完成':True}
 ]
 
 def home(request):
-    global lit
+    result = TODO.objects.all()
     if request.method=='POST':
         content=request.POST.get('待办事项')
         if not content or content.strip()=='':
             return render(request,'tulist/home.html',{'清单':lit,'警告':'请输入内容'})
         else:
-            lit.append({'待办事项':content,'已完成':False})
-            return render(request, 'tulist/home.html', {'清单': lit})
+            TODO.objects.create(thing=content, done=False)
+            result = TODO.objects.all()
+            return render(request, 'tulist/home.html', {'清单':result})
     else:
-        return render(request,'tulist/home.html',{'清单':lit})
+        return render(request,'tulist/home.html',{'清单':result})
 
 
 def edit(request,forloop_counter):
-    global lit
-    print(type(forloop_counter))
-    content=lit[forloop_counter-1]
-
     #提交发的话是post请求
     if request.method=='POST':
         content=request.POST.get("已修改事项")
         if not content or content.strip() == '':
             return render(request, 'tulist/edit.html', {'警告':'请输入内容'})
         else:
-            lit[forloop_counter-1]['待办事项']=content
+            a=TODO.objects.get(pk=forloop_counter)
+            a.thing=content
+            a.save()
             return redirect('tulist:主页')
     else:
-        content = lit[forloop_counter-1]['待办事项']
+        content = TODO.objects.get(pk=forloop_counter)
         return render(request,'tulist/edit.html',{'待修改事项':content})
 
 def about(request):
     return render(request, 'tulist/about.html')
 
 def delete(request,forloop_counter):
-    global lit
     #只能在post请求下才能删除
     if request.method=='POST':
         #防止在urls上直接做修改就能删除
-        forloop_counter = int(forloop_counter) - 1
-        print(forloop_counter)
-        lit.pop(forloop_counter)
+        TODO.objects.get(pk=int(forloop_counter)).delete()
     return redirect('tulist:主页')
 
 
